f7969735dc5f89ac785a3e31f281366298b34eaa
AVELLAN Romain <romain.avellan13@gmail.com>
2024-11-21 15:11:00 +0100

    test

--- action.py
+++ action.py
@@ -3,6 +3,60 @@ from utils import Position
 
 
 class Action:
+    def __init__(self, printer):
+        self.printer=printer
+
+    #general comande function: cmd c'est une chaine de character en g code de type "G1 X10 Y18 Z10 E10"
+    def cmd(self, cmd):
+        comand=cmd+"\n"
+        self.printer.write(comand.encode())#encoder la comande
+        respons= self.printer.readline().decode().strip()#afficher la reponse du printer
+        print(respons)
+        
+        while respons.lower() != 'ok':# attendre que la comande soit finis
+            respons= self.printer.readline().decode().strip()#afficher la reponse du printer
+            print(respons)
+
+    # These function has to convert position & high into commands
+    def goto(self, position, high):
+        """
+        Move to max_high
+        Move to position
+        Move to high
+        """
+        maxheight=str(50)
+        x=str(position[0])
+        y=str(position[1])
+        z=str(high)
+
+        self.cmd(f"G1 Z{maxheight}")
+        self.cmd(f"G1 X{x} Y{y}")
+        self.cmd(f"G1 Z{z}")
+
+
+        print("Go to ", position, high)
+        pass
+
+
+    def pippet(well, volume):
+        # Generate command for take
+
+        well.volume -= volume
+        print(f"Take volume {volume}")
+        print(well.volume)
+
+
+    def drop(well, volume):
+        # Generate command for drop
+        well.volume += volume
+        print(f"Drop volume {volume}")
+        print(well.volume)
+
+
+    def eject():
+        # Generate command for eject
+        print(f"Eject")
+
     def move_volume(self, well_from: Well, well_to: Well, volume: float):
         """Move some volume from one well to another"""
         initial_coord = well_from.module.get_well_coord(well_from.index)
@@ -19,34 +73,4 @@ class Action:
         self.goto(module.next_tip)
 
 
-# These function has to convert position & high into commands
 
-
-def goto(position, high):
-    """
-    Move to max_high
-    Move to position
-    Move to high
-    """
-    print("Go to ", position, high)
-    max_high = ...
-    pass
-
-
-def take(well, volume):
-    # Generate command for take
-    well.volume -= volume
-    print(f"Take volume {volume}")
-    print(well.volume)
-
-
-def drop(well, volume):
-    # Generate command for drop
-    well.volume += volume
-    print(f"Drop volume {volume}")
-    print(well.volume)
-
-
-def eject():
-    # Generate command for eject
-    print(f"Eject")
--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from action import Action
 from layout import init_layout
 
+import serial
+from past.builtins import raw_input
+
 if __name__ == "__main__":
+    printer=serial.Serial('COM3',115200)
+
     modules = init_layout()
     module = modules[0]
 
-    action = Action()
-    action.move_volume(module.wells[0], module.wells[1], 100)
+    action = Action(printer)
+    #test run pour mesurer les mouvement
+    # action.cmd("G1 Z40")
+    # action.goto([10,10],10)
+    
+
+    # action.move_volume(module.wells[0], module.wells[1], 100)
 
     # action.change_tips()
