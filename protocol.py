3ae92f162ffa0067a8e789f1dcdf8f804313a291
AVELLAN Romain <romain.avellan13@gmail.com>
2024-11-24 19:47:54 +0100

    protocol test

--- protocol.py
+++ protocol.py
@@ -1,11 +1,5 @@
-
-
 def run(action):
     match action["name"]:
         case "load":
-            cmd=f"action.load("+action["well"]+","+ action["volume"]+")"
+            cmd = f"action.load(" + action["well"] + "," + action["volume"] + ")"
             exec(cmd)
-        case "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday":
-            print(f"{day} is a weekday.")
-        case _:
-            print("That's not a valid day of the week.")
